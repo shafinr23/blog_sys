9f3b6624eae4a7b1535138a8c7986e088257bd98
shafin rahman <shafinr23@gmail.com>
2018-08-23 17:33:29 +0600

    make DB and  seed

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'role_id' => '1',
+            'name' => 'MD.Admin',
+            'username' => 'admin',
+            'email' => 'shafinr23@gmail.com',
+            'password' => bcrypt('root')
+
+
+        ]);
+
+         DB::table('users')->insert([
+             'role_id' => '2',
+             'name' => 'MD.author',
+             'username' => 'author',
+             'email' => 'shafinr397@gmail.com',
+             'password' => bcrypt('rootauthor')
+
+
+        ]);
+    }
+}
