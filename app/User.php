51ac7a29a2468dea39e7606e8b1c5e961b837946
shafin rahman <shafinr23@gmail.com>
2018-08-23 20:56:28 +0600

    Auth compleate , admin and author

--- app/User.php
+++ app/User.php
@@ -28,6 +28,6 @@ class User extends Authenticatable
     ];
     public function role()
     {
-        return $this->belongsTo( 'app\Role');
+        return $this->belongsTo( 'App\Role');
     }
 }
