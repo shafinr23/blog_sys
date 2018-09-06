16e23d6a6b6e46c28b3ba55ac6cba15d31c7d812
shafin rahman <shafinr23@gmail.com>
2018-09-07 00:43:22 +0600

    add all tag desing

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -316,4 +316,21 @@
     </div>
 @endsection
 @push('js')
+    <script src="{{ asset('assets/backend/js/pages/index.js') }}"></script>
+    <!-- Jquery CountTo Plugin Js -->
+    <script src="{{ asset('assets/backend/plugins/jquery-countto/jquery.countTo.js') }}"></script>
+
+    <!-- Morris Plugin Js -->
+    <script src="{{ asset('assets/backend/plugins/raphael/raphael.min.js') }}"></script>
+    <script src="{{ asset('assets/backend/plugins/morrisjs/morris.js') }}"></script>
+
+    <!-- ChartJs -->
+    <script src="{{ asset('assets/backend/plugins/chartjs/Chart.bundle.js') }}"></script>
+
+    <!-- Flot Charts Plugin Js -->
+    <script src="{{ asset('assets/backend/plugins/flot-charts/jquery.flot.js') }}"></script>
+    <script src="{{ asset('assets/backend/plugins/flot-charts/jquery.flot.resize.js') }}"></script>
+    <script src="{{ asset('assets/backend/plugins/flot-charts/jquery.flot.pie.js') }}"></script>
+    <script src="{{ asset('assets/backend/plugins/flot-charts/jquery.flot.categories.js') }}"></script>
+    <script src="{{ asset('assets/backend/plugins/flot-charts/jquery.flot.time.js') }}"></script>
 @endpush
--- resources/views/layouts/backend/partial/sidebar.blade.php
+++ resources/views/layouts/backend/partial/sidebar.blade.php
@@ -45,6 +45,12 @@
                         <span>Dashboard</span>
                     </a>
                 </li>
+                <li class="{{ Request::is('admin/tag')? 'active':'' }}">
+                    <a href="{{ route('admin.tag.index') }}">
+                        <i class="material-icons">label</i>
+                        <span>Tag</span>
+                    </a>
+                </li>
                 <li class="header"> system </li>
                 <li class="">
                     <a class="dropdown-item" href="{{ route('logout') }}"
